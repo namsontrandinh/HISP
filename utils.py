2962cc2d9919b3e0fe135caff0dfdf8634169140
namsontrandinh <namson12a0111@gmail.com>
2026-09-19 22:20:21 +0700

    update code

--- utils.py
+++ utils.py
@@ -17,6 +17,34 @@ import cupyx.scipy.sparse
 from cupy.cuda import cublas, cusparse
 import gc
 
+from estimators import exact_diag, PathSampler, hisp_vec
+
+
+def compute_structural_features(W, q, mode, s=1000, T=1000, rng=None):
+    """DIEM WIRE-UP giua MONSTOR+ va HISP (Strategy Pattern).
+
+    Chon 1 trong 3 cach tinh c_r(v), r=2..q. Ca 3 deu tra ve mang
+    (n, q-1) CUNG mot dinh dang (cot j ung voi r=j+2) - vi vay hoan
+    doi cho nhau ma khong can sua gi o cho goi ham.
+
+    mode: 'exact' (MONSTOR+ goc, tinh dung, cham) | 'est1' (HISP
+          Algorithm 1, path-sampling) | 'est2' (HISP Algorithm 2,
+          HISP-Vec).
+    s, T: so mau/probe cho est1/est2 - khong dung neu mode='exact'.
+    """
+    if rng is None:
+        rng = np.random.default_rng(0)
+    if mode == "exact":
+        return exact_diag(W, q)
+    elif mode == "est1":
+        return PathSampler(W).estimate_all(q, s, rng)
+    elif mode == "est2":
+        return hisp_vec(W, q, T, rng)
+    else:
+        raise ValueError(
+            f"feature_mode khong hop le: '{mode}' - phai la "
+            f"'exact', 'est1', hoac 'est2'")
+
 
 def _load_data_inner(train, val, test):
     base_path = './datadir'
@@ -43,7 +71,9 @@ def _load_data_inner(train, val, test):
     return objs['train'], objs['val'], objs['test']
 
 
-def load_data(train_labels, val_labels, test_labels, k=None):
+def load_data(train_labels, val_labels, test_labels, k=None,
+              feature_mode='exact', est_s=1000, est_T=1000, feature_seed=0):
+    rng = np.random.default_rng(feature_seed)
     g, sX, sy, X, y = {}, {}, {}, {}, {}
     _g, _sX, _sy = {}, {}, {}
     g1, _g1 = {}, {}
@@ -59,13 +89,9 @@ def load_data(train_labels, val_labels, test_labels, k=None):
 
         ## Creates a new graph from an adjacency matrix given as a SciPy sparse matrix.
 
-        current = copy.deepcopy(data[0])
-        rr = []
-        for i in tqdm.trange(10):
-            current = current @ data[0]
-            rr.append(current.diagonal())
-
-        g1[lstr].ndata['idfeat'] = torch.from_numpy(np.float32(np.stack(rr, axis=-1))).cuda()
+        rr = compute_structural_features(
+            data[0], q=11, mode=feature_mode, s=est_s, T=est_T, rng=rng)
+        g1[lstr].ndata['idfeat'] = torch.from_numpy(np.float32(rr)).cuda()
         g1[lstr].edata['weight'] = torch.from_numpy(np.float32(data[0].data)).cuda()
 
         X[lstr] = torch.FloatTensor(data[1])
@@ -80,12 +106,9 @@ def load_data(train_labels, val_labels, test_labels, k=None):
         _g[lstr] = dgl.from_scipy(data[0])
         _g1[lstr] = _g[lstr].to(torch.device('cuda:0'))
 
-        current = copy.deepcopy(data[0])
-        rr = []
-        for i in tqdm.trange(10):
-            current = current @ data[0]
-            rr.append(current.diagonal())
-        _g1[lstr].ndata['idfeat'] = torch.from_numpy(np.float32(np.stack(rr, axis=-1))).cuda()
+        rr = compute_structural_features(
+            data[0], q=11, mode=feature_mode, s=est_s, T=est_T, rng=rng)
+        _g1[lstr].ndata['idfeat'] = torch.from_numpy(np.float32(rr)).cuda()
         _g1[lstr].edata['weight'] = torch.from_numpy(np.float32(data[0].data)).cuda()
 
         _sX[lstr] = torch.FloatTensor(data[1])
@@ -98,12 +121,9 @@ def load_data(train_labels, val_labels, test_labels, k=None):
         _g[lstr] = dgl.from_scipy(data[0])
         _g1[lstr] = _g[lstr].to(torch.device('cuda:0'))
 
-        current = copy.deepcopy(data[0])
-        rr = []
-        for i in tqdm.trange(10):
-            current = current @ data[0]
-            rr.append(current.diagonal())
-        _g1[lstr].ndata['idfeat'] = torch.from_numpy(np.float32(np.stack(rr, axis=-1))).cuda()
+        rr = compute_structural_features(
+            data[0], q=11, mode=feature_mode, s=est_s, T=est_T, rng=rng)
+        _g1[lstr].ndata['idfeat'] = torch.from_numpy(np.float32(rr)).cuda()
         _g1[lstr].edata['weight'] = torch.from_numpy(np.float32(data[0].data)).cuda()
 
         _sX[lstr] = torch.FloatTensor(data[1])
